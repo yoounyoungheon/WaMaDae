3bc3733ccf2677ad06b8ccd547413d3345b312b2
yoounyoungheon <tbvjgjsl9@naver.com>
2025-04-17 00:08:53 +0900

    [refactor] 이미지 최적화

--- web/src/app/ui/component/customer/meeting-view.tsx
+++ web/src/app/ui/component/customer/meeting-view.tsx
@@ -1,30 +1,33 @@
 import { Meeting } from "@/app/business/meeting/meeting.domain";
 import { Card, CardTitle } from "../../molecule/card";
 import Link from "next/link";
+import Image from "next/image";
 
 interface MeetingViewProps {
-  meeting: Meeting
+  meeting: Meeting;
 }
 
-export function MeetingView({meeting}:MeetingViewProps){
-
+export function MeetingView({ meeting }: MeetingViewProps) {
   return (
     <Link href={`/customer/book?meetingId=${meeting.id}`}>
-      <Card
-      className="relative p-3 border-none shadow-lg rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300"
-      style={{ backgroundImage: `url(${meeting.imgUrl})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
-      >
-      <div className="absolute inset-0 bg-gradient-to-r from-gray-900 via-gray-900/40 to-transparent z-0" />
-      <div className="relative z-10">
-        <CardTitle className="text-lg font-bold text-white">
-          {meeting.meetingName}
-        </CardTitle>
-        <div className="p-10" />
-        <div className="mt-auto inline-block text-white text-sm">
-          {`자세히 보기 >`}
+      <Card className="relative p-3 border-none shadow-lg rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300">
+        <Image
+          src={meeting.imgUrl}
+          alt={meeting.meetingName}
+          fill
+          className="object-cover object-center z-0"
+        />
+        <div className="absolute inset-0 bg-gradient-to-r from-gray-900 via-gray-900/40 to-transparent z-10" />
+        <div className="relative z-20">
+          <CardTitle className="text-lg font-bold text-white">
+            {meeting.meetingName}
+          </CardTitle>
+          <div className="p-10" />
+          <div className="mt-auto inline-block text-white text-sm">
+            자세히 보기 &gt;
+          </div>
         </div>
-      </div>
       </Card>
     </Link>
-  )
+  );
 }
