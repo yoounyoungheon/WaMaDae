b886e8ebf0a323535004ce602375d8557fa2b421
yoounyoungheon <tbvjgjsl9@naver.com>
2025-05-29 17:10:15 +0900

    [feat] 예약 현황 view

--- web/src/app/(page)/manager/book/page.tsx
+++ web/src/app/(page)/manager/book/page.tsx
@@ -3,30 +3,31 @@ import { loadBooksByMeetingId } from "@/app/business/book/book.service";
 import { BookInfo } from "@/app/ui/component/manager/bookInfo";
 
 export default async function ManagerBookPage({
-    searchParams,
+  searchParams,
   }: {
-    searchParams: { [key: string]: string | undefined };
+  searchParams: { [key: string]: string | undefined };
   }){
   const id = searchParams.id;
   let books: Book[] | null = null;
 
   if(typeof id !== 'undefined'){
-    const response = await loadBooksByMeetingId(id);
-    console.log(response)
-    if(response.isSuccess && response.data){
-      books = response.data;
-    }
+  const response = await loadBooksByMeetingId(id);
+  console.log(response)
+  if(response.isSuccess && response.data){
+    books = response.data;
+    books.sort((a, b) => a.name.localeCompare(b.name));
+  }
   }
 
   return (
-  <main className="p-10 grid grid-cols-4">
-    {books&&(
-      books.map((book, index)=>{
-        return (
-          <BookInfo key={index} book={book} isManager={true}/>
-        )
-      })
-    )}
+  <main className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+  {books && (
+    books.map((book, index) => {
+      return (
+        <BookInfo key={index} book={book} isManager={true} />
+      );
+    })
+  )}
   </main>
   )
 }
