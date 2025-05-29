307079fe9e2b97cfe5dc950ffe33285a922a52fa
yoounyoungheon <tbvjgjsl9@naver.com>
2025-05-29 17:00:23 +0900

    [feat] book list view 수정

--- web/src/app/ui/component/customer/book-list.view.tsx
+++ web/src/app/ui/component/customer/book-list.view.tsx
@@ -8,13 +8,10 @@ export function BookListView({ books }: { books: Book[] }) {
 
   return (
     <main className="px-4">
-      <div className="text-lg font-semibold mb-1">예약자 현황</div>
-      <p className="text-sm text-slate-500 mb-5">
-        입금이 완료된 예약자만 표시됩니다.
-      </p>
+      <div className="text-lg font-semibold mb-3">📖 예약자 현황 </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {bookedMembers.map((bookedMember, index) => (
-          <div
+          bookedMember.nickname && <div
             key={index}
             className="bg-white rounded-2xl shadow p-4 border border-slate-200 hover:shadow-md transition"
           >
